0b04e2f97741ba7fe5b7410c06d14b06f6ce23d8
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-13 16:00:04 +0530

    Create Arithmetic Number.java

---
+++ February-2023/Easy/Arithmetic Number.java
@@ -0,0 +1,37 @@
+/******************************************************************************************************************************************************
+
+Given three integers  'A' denoting the first term of an arithmetic sequence , 'C' denoting the common difference of an arithmetic sequence and an integer 'B'.
+you need to tell whether 'B' exists in the arithmetic sequence or not. Return 1 if B is present in the sequence. Otherwise, returns 0.
+
+---------------------------------------------------------------------------------------
+
+Example 1:
+
+Input: A = 1, B = 3, C = 2
+Output: 1
+Explaination: 3 is the second term of the 
+sequence starting with 1 and having a common 
+difference 2.
+Example 2:
+
+Input: A = 1, B = 2, C = 3
+Output: 0
+Explaination: 2 is not present in the sequence.
+
+----------------------------------------------------------------------------------------
+
+*******************************************************************************************************************************************************/
+
+//User function Template for Java
+
+class Solution{
+    static int inSequence(int a, int b, int c){
+        // code here
+        
+        if(a==b)    return 1;
+        if(c==0)    return 0;
+        if((b-a)%c==0 && (b-a)/c>=0)    return 1;
+        
+        return 0;
+    }
+}
